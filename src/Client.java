cfd85adda2519da6590ffed0ea596a6c6eb2e8e1
Sophie B <105296740+SofiSoap@users.noreply.github.com>
2022-11-28 20:26:48 -0500

    Add files via upload

---
+++ src/Client.java
@@ -0,0 +1,69 @@
+
+import java.net.*;
+
+import javax.swing.JFrame;
+
+import java.io.*;
+ 
+public class Client
+{
+
+    private Socket socket            = null;
+    private DataInputStream  input   = null;
+    private DataOutputStream out     = null;
+ 
+    
+    public Client(String address, int port) {
+      
+        try{
+            socket = new Socket(address, port);
+            System.out.println("Connected");
+ 
+           
+            input  = new DataInputStream(System.in);
+ 
+          
+            out    = new DataOutputStream(socket.getOutputStream());
+        }
+        catch(UnknownHostException u) {
+            System.out.println(u);
+        }
+        catch(IOException i) {
+            System.out.println(i);
+        }
+ 
+     
+        String line = "";
+ 
+       
+        while (!line.equals("Over")) {
+            try {
+                line = input.readLine();
+                out.writeUTF(line);
+            }
+            catch(IOException i){
+                System.out.println(i);
+            }
+        }
+ 
+   
+        try{
+            input.close();
+            out.close();
+            socket.close();
+        }
+        catch(IOException i){
+            System.out.println(i);
+        }
+    }
+ 
+    public static void main(String args[]){
+        Client client = new Client("127.0.0.1", 5000);
+        
+        JFrame frame = new JFrame()	;
+        frame.setTitle("Server");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500); //sets x and y dimension
+        frame.setVisible(true);
+    }
+}
